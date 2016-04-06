8061c1659ac83b4bed4cad47b6ca3b1c2dd783dd
Your Name <you@example.com>
2016-04-06 15:17:52 +0400

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,9 +73,6 @@
     </nav>
 
     @yield('content')
-    @yield('content')
-    @yield('content')
-    @yield('content')
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
